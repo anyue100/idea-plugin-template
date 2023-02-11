0c43e0fe4790470629abf633a623fc4f6c42ca18
GitHub Action <action@github.com>
2023-02-11 03:44:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/anyue100/ideaplugintemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/anyue100/ideaplugintemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.anyue100.ideaplugintemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.anyue100.ideaplugintemplate.MyBundle
 
 class MyApplicationService {
 
